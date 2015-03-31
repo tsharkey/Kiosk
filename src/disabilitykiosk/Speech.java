409fa218c5b916d3be95f7f4a890610db5ebda3c
Viet Dinh <hoang_viet1985@yahoo.com>
2015-03-30 20:57:14 -0400

    Speech button overlap each other when multiple speech text are clicked.(FIXED)

--- src/disabilitykiosk/Speech.java
+++ src/disabilitykiosk/Speech.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package disabilitykiosk;
 
 import com.sun.speech.freetts.VoiceManager;
@@ -15,32 +14,40 @@ import java.util.logging.Logger;
  * @author Eric Sullivan
  */
 public class Speech implements Runnable {
+
     String text;
     public static Speech instance = null;
-    
-    public Speech(String text){
+    public static boolean allowToSpeech = true;
+
+    public Speech(String text) {
         this.text = text;
     }
-    public static Speech getInstance(String text){
-        if(instance == null)
+
+    public static Speech getInstance(String text) {
+        if (instance == null) {
             instance = new Speech(text);
-        
+        }
+
         return instance;
     }
-    public void run(){
-        VoiceManager vm = VoiceManager.getInstance();
-        com.sun.speech.freetts.Voice voice = vm.getVoice("kevin16");
-        String string[] = text.split("  ");
-        voice.allocate();
-        for (int i = 0; i < string.length; i++) {
-               voice.speak(text);
-        voice.deallocate();
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException ex) {
-                Logger.getLogger(Speech.class.getName()).log(Level.SEVERE, null, ex);
+
+    public void run() {
+        if (allowToSpeech == true) {
+            allowToSpeech = false;
+            VoiceManager vm = VoiceManager.getInstance();
+            com.sun.speech.freetts.Voice voice = vm.getVoice("kevin16");
+            String string[] = text.split("  ");
+            voice.allocate();
+            for (int i = 0; i < string.length; i++) {
+                voice.speak(text);
+                voice.deallocate();
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(Speech.class.getName()).log(Level.SEVERE, null, ex);
+                }
             }
+            allowToSpeech = true;
         }
-        
-}
+    }
 }
