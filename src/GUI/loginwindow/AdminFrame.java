2574d9645cffab40ec06664968f234e0bf57df9c
Catherine Huang <c_huang4@post.massbay.edu>
2015-04-08 13:58:41 -0400

    nothing important" "

--- src/GUI/loginwindow/AdminFrame.java
+++ src/GUI/loginwindow/AdminFrame.java
@@ -22,19 +22,21 @@ import java.io.IOException;
 
 public class AdminFrame extends JFrame {
 
- private final int WINDOW_WIDTH = 500;
+	private final int WINDOW_WIDTH = 500;
     private final int WINDOW_HEIGHT = 100;
 
+    private JButton launchKioskButton = new JButton("LAUNCH KIOSK");
     private JButton reportButton = new JButton("VIEW REPORT");
     private JButton closeKioskButton = new JButton("CLOSE KIOSK");
     private JButton createNewAdmin = new JButton("NEW ADMIN");//allow the admin to create another admin
     private JButton specialist = new JButton("ADD SPECIALIST"); //creates a adddeletespecframe -Brendan S
     private JButton logout = new JButton("LOG OUT");
-    
+
     public AdminFrame() {
 
         setLayout(new GridLayout());
         JPanel button = new JPanel();
+        button.add(launchKioskButton);
         button.add(createNewAdmin);
         button.add(closeKioskButton);
         button.add(reportButton);
@@ -49,16 +51,20 @@ public class AdminFrame extends JFrame {
         ReportButtonListener onClick = new ReportButtonListener();
         getReportButton().addActionListener(onClick);
 
+        // launch kiosk button link
+        LaunchKioskButtonListener click = new LaunchKioskButtonListener();
+        getLaunchKioskButton().addActionListener(click);
+
         // close kiosk button link
         CloseKioskButtonListener close = new CloseKioskButtonListener();
         getCloseKioskButton().addActionListener(close);
-        
+
         //button helps creating another admin account
         getCreateAdminButton().addActionListener(new CreateAdminButtonListener());
-        
-        //Brendan S 
+
+        //Brendan S
         getSpecialistButton().addActionListener(new SpecialistButtonListener());
-        
+
         //log out
         getLogoutButton().addActionListener(new LogoutButtonListener());
 
@@ -66,7 +72,7 @@ public class AdminFrame extends JFrame {
         getRootPane().setWindowDecorationStyle(JRootPane.WHEN_IN_FOCUSED_WINDOW);
         //setAlwaysOnTop(true);
         setResizable(false);
-//        getRootPane().setDefaultButton(launchKioskButton);
+        getRootPane().setDefaultButton(launchKioskButton);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
@@ -76,29 +82,42 @@ public class AdminFrame extends JFrame {
         return reportButton;
     }
 
+    public final JButton getLaunchKioskButton() {
+        return launchKioskButton;
+    }
+
     public final JButton getCloseKioskButton() {
         return closeKioskButton;
     }
-    
+
     public final JButton getCreateAdminButton() {
         return createNewAdmin;
     }
-    
+
     public final JButton getLogoutButton(){
         return logout;
     }
-    
+
     public final JButton getSpecialistButton() {
-  return specialist;
- }
+		return specialist;
+	}
 
 
     // action listeners for each button
+    public class LaunchKioskButtonListener implements ActionListener {
+        public void actionPerformed(ActionEvent ae) {
+            if (ae.getSource() == getLaunchKioskButton()) {
+                new DisabilityKiosk();
+                // launches Disability window
+                dispose();
+            }
+        }
+    }
 
     public class CloseKioskButtonListener implements ActionListener {
         public void actionPerformed(ActionEvent ee) {
             if (ee.getSource() == getCloseKioskButton()) {
-             Admin.serialize();
+            	Admin.serialize();
                 SpecialistList.serialize();
                 dispose();
                 // close app
@@ -114,16 +133,16 @@ public class AdminFrame extends JFrame {
             if (e.getSource() == getReportButton()) {
                 // this button goes to the report window
                 try {
-     new ReportWindow();
-    } catch (IOException e1) {
-     // TODO Auto-generated catch block
-     e1.printStackTrace();
-    }
+					new ReportWindow();
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
                 dispose();
             }
         }
     }
-    
+
     //create another admin account, and return to admin window
     private class CreateAdminButtonListener implements ActionListener {
 
@@ -135,12 +154,12 @@ public class AdminFrame extends JFrame {
             do{
                 JPanel adminInput = new JPanel();
                 adminInput.add(new JLabel("Username: " ));
-                
+
                 adminInput.add(userName);
                 adminInput.add(Box.createHorizontalStrut(15));
-                 
+
                 adminInput.add(new JLabel("Password: "));
-                
+
                 adminInput.add(password);
                 submitted = JOptionPane.showConfirmDialog(null, adminInput, "Please enter new Admin Username & Password.", JOptionPane.OK_CANCEL_OPTION);
                 if (submitted == JOptionPane.OK_OPTION && !userName.getText().equals("") && password.getPassword().length != 0) {
@@ -157,36 +176,35 @@ public class AdminFrame extends JFrame {
 
                 }
             }while(submitted == JOptionPane.OK_OPTION && (userName.getText().equals("") || password.getPassword().length == 0));
-            
+
             if(submitted == JOptionPane.CANCEL_OPTION){
                 dispose();
                 //get back to admin window
                 AdminFrame test = new AdminFrame();
-  test.setVisible(true);
+		test.setVisible(true);
             }
         }
     }
-    
+
     //current admin logs out, return to main window of the application
     private class LogoutButtonListener implements ActionListener{
 
         @Override
         public void actionPerformed(ActionEvent e) {
             Admin.isAdminWorking = false;
-            System.exit(1);
             new DisabilityKiosk();
         }
-        
+
     }
-    
+
     private class SpecialistButtonListener implements ActionListener{
 
-  @Override
-  public void actionPerformed(ActionEvent e) {
-   new AddDeleteSpecFrame();
-   
-  }
-     
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			new AddDeleteSpecFrame();
+
+		}
+
     }
     
     //we should add more buttons to give the admin more rights.
