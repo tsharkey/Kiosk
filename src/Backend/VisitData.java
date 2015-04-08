63838392ec984d2e1944a0485ce4b12c482d79cf
tsharkey <Tom@Thomas-Sharkeys-MacBook-Pro.local>
2015-04-08 11:53:42 -0400

    adding visit data class

---
+++ src/Backend/VisitData.java
@@ -0,0 +1,56 @@
+package Backend;
+
+import java.util.Date;
+
+
+/**
+ * Created by Tom on 4/8/15.
+ */
+public class VisitData{
+    private Date visitDate;
+    private Date visitTime;
+    private String reason;
+    private boolean followUp;
+    private String email;
+    private String specialist;
+    private String location;
+
+    public VisitData(Date visitDate, Date visitTime, String reason, boolean followUp, String email, String specialist, String location){
+        this.visitDate = visitDate;
+        this.visitTime = visitTime;
+        this.reason = reason;
+        this.followUp = followUp;
+        this.email = email;
+        this.specialist = specialist;
+        this.location = location;
+
+    }
+
+    public Date getVisitDate(){
+        return visitDate;
+    }
+
+    public Date getVisitTime(){
+        return visitTime;
+    }
+
+    public String getReason(){
+        return reason;
+    }
+
+    public boolean isFollowUp(){
+        return followUp;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public String getSpecialist(){
+        return specialist;
+    }
+
+    public String getLocation(){
+        return location;
+    }
+}
