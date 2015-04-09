3f6db1891b34744f558ac4e5d59e2dcbc9a98f1c
b-casey <b-casey@users.noreply.github.com>
2015-04-09 14:25:16 -0400

    Update VisitsTable.java

--- src/Backend/VisitsTable.java
+++ src/Backend/VisitsTable.java
@@ -1,6 +1,7 @@
 package Backend;
 
-import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.Date;
 /**
  *
  * @author Sean Johnston, Brendan Casey
@@ -8,20 +9,66 @@ import java.sql.ResultSet;
 public class VisitsTable {
 	
     //creates a new visit, the email must match an already existing email in the user database
-    public static void addVisit(String reason, int followUp, String email, String specialist, String location){
-    	DatabaseConnector.executeUpdate("INSERT INTO VISITS(visitDate, visitTime, reason, followUp, email, ID, specialist, location)"
-                +"VALUES(null, null, '"+reason+"', "+followUp+",'"+email+"', null, '"+specialist+"', '"+location+"')");
+    public static boolean addVisit(String reason, Boolean followUp, String email, String specialist, String location){
+    	int insertCount = DatabaseConnector.executeUpdate("INSERT INTO VISITS(reason, followUp, email, specialist, location) "
+                + "VALUES('" + reason + "', " + followUp + ", '" + email + "', '" + specialist + "', '" + location + "')");
+    	return (insertCount != 0) ? true : false;
     }
 
     //deletes a visit based on date and email
-    public static void deleteVisit(String email, String date, String time){
-    	DatabaseConnector.executeUpdate("DELETE FROM VISITS" +
+    public static boolean deleteVisit(String email, String date, String time){
+    	int deleteCount = DatabaseConnector.executeUpdate("DELETE FROM VISITS" +
                 "WHERE visitDate = '" + date + "'and visitTime = '" + time + "' and email = '" + email + "'");
+    	return (deleteCount != 0) ? true : false;
     }
 
+    /*
     //takes in a start and end date to return a set of visits between the ranges
     public static ResultSet visitsByDate(String startDate, String endDate){
         return null;//DatabaseConnector.executeQuery("SELECT * FROM VISITS"+
                 //"WHERE visitDate between '"+startDate+"' and '"+endDate+"'");
     }
+    */
+    
+    public static ArrayList<Date> getVisitDates()
+    {
+    	return DatabaseConnector.executeQueryDates("visitDate", "SELECT visitDate FROM VISITS ORDER BY ID ASC", false);
+    }
+    
+    public static ArrayList<Date> getVisitTimes()
+    {
+    	return DatabaseConnector.executeQueryDates("visitTime", "SELECT visitTime FROM VISITS ORDER BY ID ASC", true);
+    }
+    
+    public static ArrayList<Boolean> getFollowUps()
+    {
+    	return DatabaseConnector.executeQueryBools("followUp", "SELECT followUp FROM VISITS ORDER BY ID ASC");
+    }
+    
+    public static ArrayList<String> getReasons()
+    {
+    	return DatabaseConnector.executeQueryStrings("reason", "SELECT reason FROM VISITS ORDER BY ID ASC");
+    }
+    
+    public static ArrayList<String> getEmails()
+    {
+    	return DatabaseConnector.executeQueryStrings("email", "SELECT email FROM VISITS ORDER BY ID ASC");
+    }
+    
+    public static ArrayList<String> getSpecialists()
+    {
+    	return DatabaseConnector.executeQueryStrings("specialist", "SELECT specialist FROM VISITS ORDER BY ID ASC");
+    }
+    
+    public static ArrayList<String> getLocations()
+    {
+    	return DatabaseConnector.executeQueryStrings("location", "SELECT location FROM VISITS ORDER BY ID ASC");
+    }
+    
+    /*
+    public static ArrayList<VisitData> searchVisitString(String query, String column)
+    {
+    	//
+    }
+    */
 }
