2f5e464ce1907907d3baf2bca6cb87b9864372b1
b-casey <b-casey@users.noreply.github.com>
2015-04-11 16:40:34 -0400

    Update VisitData.java

--- src/Backend/VisitData.java
+++ src/Backend/VisitData.java
@@ -3,33 +3,28 @@ package Backend;
 import java.util.Date;
 
 /**
- * Created by Tom on 4/8/15.
+ * @author Tom, Brendan
  */
-public class VisitData {
+public class VisitData extends UserData {
 	private Date visitDate;
 	private Date visitTime;
 	private String reason;
 	private boolean followUp;
-	private String email;
 	private String specialist;
 	private String location;
-	private String firstName;
-	private String lastName;
-	private String phone;
 
 	public VisitData(Date visitDate, Date visitTime, String reason,
-			boolean followUp, String email, String specialist, String location,
-			String firstName, String lastName, String phone) {
+			boolean followUp, String specialist, String location,
+			String firstName, String lastName, String email, String phone,
+			String role) {
+
+		super(firstName, lastName, email, phone, role);
 		this.visitDate = visitDate;
 		this.visitTime = visitTime;
 		this.reason = reason;
 		this.followUp = followUp;
-		this.email = email;
 		this.specialist = specialist;
 		this.location = location;
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.phone = phone;
 	}
 
 	public Date getVisitDate() {
@@ -48,27 +43,11 @@ public class VisitData {
 		return followUp;
 	}
 
-	public String getEmail() {
-		return email;
-	}
-
 	public String getSpecialist() {
 		return specialist;
 	}
 
 	public String getLocation() {
 		return location;
-	}
-
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
-	
-	public String getPhone() {
-		return phone;
 	}
 }
