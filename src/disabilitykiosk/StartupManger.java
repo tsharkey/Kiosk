86c563177491013bcf241588c005143290f3c124
b-casey <scienttt@gmail.com>
2015-04-20 12:48:46 -0400

    add system property to prevent null ptr with freetts and jar

--- src/disabilitykiosk/StartupManger.java
+++ src/disabilitykiosk/StartupManger.java
@@ -11,12 +11,16 @@ public class StartupManger {
 	public static boolean initalizeDB = false;
 
 	public StartupManger() {
+		
+		System.setProperty("freetts.voices", 
+		        "com.sun.speech.freetts.en.us.cmu_us_kal.KevinVoiceDirectory");
 
 		@SuppressWarnings("unused")
 		DatabaseInitFrame dbcf = new DatabaseInitFrame();
 	}
 
 	public static void main(String[] args) {
+		
 		if (args.length != 0) {
 			if (args[0].equals("initalizeDatabase")) {
 				initalizeDB = true;
